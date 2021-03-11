f2dfc2ab91bb3a5530367e13059d537485094853
Marcos Melo <marcaos.melo@gmail.com>
2021-03-11 11:27:50 +0000

    Create dino.js

---
+++ js/dino.js
@@ -0,0 +1,24 @@
+class Dino {
+    constructor() {
+        this.r = 50; // reuso do valor
+        this.x = this.r; // localização x
+        this.y = height - this.r; // localização y - altura
+        this.vy = 0; // velocidade Jump;
+        this.gravity = 2;
+    }
+
+    // jump() {
+    //     this.vy = -25;
+    // }
+
+    // move() {
+    //     this.y += this.vy;
+    //     this.vy += this.gravity;
+    //     this.y = constrain(this.y, 0, height - this.r); // Limitar o valor de y entre 0 e altura - menos tamanho objeto
+    // }
+
+    show() {
+        //rect(this.x, this.y, this.r, this.r);
+        image(dImg, this.x, this.y, this.r, this.r);
+    }
+}
