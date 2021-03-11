de89faa20b90cc9852a9471d7de3b01649891e96
Marcos Melo <marcaos.melo@gmail.com>
2021-03-11 18:26:01 +0000

    Update dino.js

--- js/dino.js
+++ js/dino.js
@@ -8,18 +8,21 @@ class Dino {
     }
 
     jump() {
+        console.log('salta dino');
         this.vy = -25;
-        console.log('jump');
     }
 
     move() {
+        console.log('move dino');
         this.y += this.vy;
+        console.log(this.y);
         this.vy += this.gravity;
         //this.y = constrain(this.y, 0, height - this.r); // Limitar o valor de y entre 0 e altura - menos tamanho objeto
     }
 
     show() {
         //myRect(this.x, this.y, this.r, this.r);
+        console.log(this.y);
         myImage(dImg, this.x, this.y, this.r, this.r);
     }
 }
